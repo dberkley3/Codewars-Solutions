55e32a674574cfddc715de63054a71cf314531e3
dberkley3 <dberkley3@gmail.com>
2022-07-27 13:12:22 -0400

    8kyu

---
+++ 8 KYU/Make Uppercase.js
@@ -0,0 +1,10 @@
+/* INSTRUCTIONS 
+
+Write a function which converts the input string to uppercase. */
+
+//ANSWER
+
+function makeUpperCase(str) {
+    // Code here
+    return str.toUpperCase()
+  }
