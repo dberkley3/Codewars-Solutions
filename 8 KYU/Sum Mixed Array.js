f1c810467692b518359dd47bba1ea9b5c5e015b1
dberkley3 <dberkley3@gmail.com>
2022-08-19 15:46:54 -0400

    8kyu

---
+++ 8 KYU/Sum Mixed Array.js
@@ -0,0 +1,12 @@
+/* INSTRUCTIONS
+
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number. */
+
+//ANSWER
+
+function sumMix(x){
+    return x.reduce((x, y) => Number(x) + Number(y), 0)
+   }
+   
