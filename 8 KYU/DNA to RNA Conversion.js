f4c7e0c5319238877319f458680b6bf6e82584a0
dberkley3 <dberkley3@gmail.com>
2022-08-18 21:57:07 -0400

    8kyu

---
+++ 8 KYU/DNA to RNA Conversion.js
@@ -0,0 +1,22 @@
+/* INSTRUCITONS
+
+Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+Create a function which translates a given DNA string into RNA. */
+
+//ANSWER 
+
+function DNAtoRNA(dna) {
+    let str = '';
+    for (let i = 0; i < dna.length; i++){
+      if (dna[i] == 'T'){
+      str += 'U';
+      } else {
+      str += dna[i];
+      }
+    }
+    return str;
+   }
+   
