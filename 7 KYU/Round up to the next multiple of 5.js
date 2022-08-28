3ed17aaf0b85267d9437a352e3be1cb2e9847480
dberkley3 <dberkley3@gmail.com>
2022-08-27 22:58:17 -0400

    7kyu

---
+++ 7 KYU/Round up to the next multiple of 5.js
@@ -0,0 +1,9 @@
+/* INSTRUCTIONS
+
+Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5? */
+
+// ANSWER
+
+function roundToNext5(n){
+    return Math.ceil(n/5)*5
+  }
