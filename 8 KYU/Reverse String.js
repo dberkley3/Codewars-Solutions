95880d33104a2be1991054bb69ba8a886526655c
dberkley3 <dberkley3@gmail.com>
2022-09-04 22:15:38 -0400

    8kyu

---
+++ 8 KYU/Reverse String.js
@@ -0,0 +1,9 @@
+/* INSTRUCTIONS 
+
+Complete the solution so that it reverses the string passed into it. */
+
+//ANSWER
+
+function solution(str){
+    return str.split('').reverse().join('');  
+  }
