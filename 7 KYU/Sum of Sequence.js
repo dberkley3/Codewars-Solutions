14abf6f48844d26f20d1f2f25fe33f777192437f
dberkley3 <dberkley3@gmail.com>
2022-09-12 21:51:10 -0400

    7kyu

---
+++ 7 KYU/Sum of Sequence.js
@@ -0,0 +1,17 @@
+/* INSTRUCTIONS
+
+Your task is to make function, which returns the sum of a sequence of integers.
+
+The sequence is defined by 3 non-negative values: begin, end, step (inclusive).
+
+If begin value is greater than the end, function should returns 0. */
+
+//ANSWER
+
+const sequenceSum = (begin, end, step) => {
+    let count = 0;
+    for (let i = begin; i <= end; i += step) {
+      count += i;
+    }
+    return count;
+  };
