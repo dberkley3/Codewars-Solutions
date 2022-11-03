6aa45d932aeb8a6b886b2b093eaf01c057f8264d
dberkley3 <dberkley3@gmail.com>
2022-11-02 20:53:36 -0400

    8kyu

---
+++ 8 KYU/How many lightsabers do you own.js
@@ -0,0 +1,17 @@
+/* INSTRUCTIONS
+
+Inspired by the development team at Vooza, write the function that
+
+accepts the name of a programmer, and
+returns the number of lightsabers owned by that person.
+The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0. */
+
+//ANSWER
+
+function howManyLightsabersDoYouOwn(name) {
+    if(name === 'Zach'){
+      return 18
+    }else{
+      return 0
+    }
+  }
