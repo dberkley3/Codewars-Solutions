9770dd202303798eba89e39189558b2b90e02216
dberkley3 <dberkley3@gmail.com>
2022-11-10 20:08:06 -0500

    8kyu

---
+++ 8 KYU/Grasshopper - Summation.js
@@ -0,0 +1,12 @@
+/* INSTRUCTIONS
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0. */
+
+//ANSWER
+
+const summation = function (num) {
+    let sum = 0
+    for (i = num; i >= 1 ; i--){
+      sum += i
+    }
+    return sum
+  }
