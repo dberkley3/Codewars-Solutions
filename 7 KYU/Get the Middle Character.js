0f20dffa60813e64589ddeb8b94039335005ddd1
dberkley3 <dberkley3@gmail.com>
2022-11-11 22:15:32 -0500

    7kyu

---
+++ 7 KYU/Get the Middle Character.js
@@ -0,0 +1,13 @@
+/* INSTRUCTIONS
+You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+*/
+
+//ANSWER
+
+function getMiddle(s) {
+    let middle = Math.floor(s.length/2);
+       if (s.length % 2 == 0)
+           return s[middle-1] + s[middle];
+       else
+           return s[middle];
+   }
