fb568c23c4a7eb6c521226fde9a1fe37a3276b80
dberkley3 <dberkley3@gmail.com>
2022-11-16 16:29:18 -0500

    6kyu

---
+++ 6 KYU/Persistant Bugger.js
@@ -0,0 +1,13 @@
+/* INSTRUCTIONS
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit. */
+
+//ANSWER
+
+function persistence(num) {
+    let i = 0;
+    while (num.toString().length !== 1) {
+      num = num.toString().split("").reduce((a,b)=>a*b);
+      i++;
+    }
+    return i;
+ }
