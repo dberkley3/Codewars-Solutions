7229ff8e05cc7d803e8d1a163ddb70ea23d39bfc
dberkley3 <dberkley3@gmail.com>
2022-11-30 17:22:24 -0500

    8kyu

---
+++ 8 KYU/Expressions Matter.js
@@ -0,0 +1,15 @@
+/* INSTRUCTIONS
+Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained */
+
+//ANSWER
+
+function expressionMatter(a, b, c) {
+    let output = [];
+    output.push(a *(b + c));
+    output.push(a * b * c);
+    output.push(a + b * c);
+    output.push((a + b) * c);
+    output.push(a+b+c);
+      return Math.max(...output)
+  }
